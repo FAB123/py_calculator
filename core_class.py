419113746fdfc4bb61b32132ea8901e148aabcbd
Fysal KT <fysalkt@gmail.com>
2020-06-28 03:14:41 +0300

    Stage 2

--- core_class.py
+++ core_class.py
@@ -1,5 +1,30 @@
+from tkinter import *
 class calc_core:
+    #get key press
+    def press_me(self, key):
+        print("You Pressed " + str(key))
+        #C.create_text(100, 10, font="Verdana 10 bold", text="Hello World!" + key)
+        display = Entry(fkt, font=("Helvetica", 16), borderwidth=0, relief=RAISED, justify=RIGHT)
+        display.insert(0, key)
+        display.grid(row=0, column=0, columnspan=5)
+
+
+    #set Buttons
     def set_buttons(self):
-        cont = 10
-        for x in cont:
-            but = Button(text=x, width=15, hight=15)
+        keys = ["ON", "AC", "%", "/", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", ".", "0", "sum"]
+        n_row, n_col, t_row = 1, 0, 1
+        for x in keys:
+            if x == 'sum':
+                buttons = Button(text="=", width=13, height=3, relief=RIDGE, font=("Courier New", 8, 'bold'), bd=2,
+                                 command=lambda: self.press_me(sum))
+                buttons.config(font=('Courier New', 16, 'bold'), bg='#ffbf00', fg="white")
+                buttons.grid(row=n_row, column=n_col, columnspan=2, pady=3, padx=3)
+            else:
+                buttons = Button(text=x, width=7, height=3, relief=RIDGE, font=("Courier New", 8, 'bold'), bd=2, command=lambda x=x: self.press_me(x))
+                buttons.config(font=('Courier New', 14, 'bold'), bg='#ffe699', fg="#ff00ff")
+                buttons.grid(row=n_row, column=n_col, pady=3, padx=3)
+                if n_col == 3:
+                    buttons.config(font=('Courier New', 14, 'bold'), bg='#ffbf00', fg="white")
+                    n_row, t_row, n_col = 2 + t_row, t_row + 1, 0
+                else:
+                    n_col = n_col + 1
--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 from tkinter import *
 import core_class
 
-def press(key):
-    print("You Pressed "+ key)
+class calculator(core_class.calc_core):
+    # initializing Tkinter & Setting WindowRIDGE
+    fkt = Tk()
+    fkt.geometry("375x570")
+    fkt.title("Simple Calculator With Voice")
+    fkt.iconbitmap("calculator.ico")
+    fkt.configure(bg="white")
+    fkt.resizable(0, 0)
+    #Set Welcome Screen
+    C = Canvas(fkt, bd=2, bg="gray", height=80, width=340)
+    C.create_text(240, 40, font=("Purisa", 30), justify=RIGHT, text="Hello World!")
+    C.delete(ALL)
+    C.create_text(240, 40, font=("Purisa", 30), text="Hi!", justify=RIGHT)
+    C.grid(rowspan=1, columnspan=5, padx=1, pady=15)
 
-
-def set_buttons():
-    cont = ["OK", "1"]
-    for x in cont:
-        but = Button(text=x, width=8, height=3, padx=2, pady=2, relief=RIDGE, command=press(x))
-        but.pack()
-
-#initializing Tkinter & Setting WindowRIDGE
-fkt=Tk()
-fkt.geometry("350x400")
-fkt.title("Simple Calculator With Voice")
-fkt.iconbitmap("calculator.ico")
-fkt.configure(bg="white")
-C = Canvas(fkt, bd=2, bg="gray", height=80, width=340)
-C.pack()
-
-set_buttons()
-
-
-
-fkt.mainloop()
+    #call core engine
+    engine = core_class.calc_core()
+    engine.set_buttons()
+    #call Gui
+    fkt.mainloop()
