4efa8e023cd73e818a062400477c4db9e643d716
Fysal <fysalkt@gmail.com>
2020-06-28 14:03:50 +0300

    step 2

--- calc_core.py
+++ calc_core.py
@@ -1,13 +1,34 @@
 from tkinter import *
+import pyttsx3
+
 class calc_core:
+    def __init__(self):
+        self.keys = "0"
+
+    def dispaly(self, fkt):
+        # Set Welcome Screen
+        self.C = Canvas(fkt, bd=2, bg="gray", height=80, width=340)
+        self.C.create_text(250, 40, font=("Purisa", 30), justify=RIGHT, text="Hello World!")
+        self.C.grid(rowspan=1, columnspan=5, padx=1, pady=15)
+
     #get key press
+    def say(self, key):
+        sound = pyttsx3.init()
+        sound.say(key)
+        sound.setProperty('rate', 10)
+        sound.runAndWait()
+
     def press_me(self, key):
         print("You Pressed " + str(key))
-        #C.create_text(100, 10, font="Verdana 10 bold", text="Hello World!" + key)
-        display = Entry(fkt, font=("Helvetica", 16), borderwidth=0, relief=RAISED, justify=RIGHT)
-        display.insert(0, key)
-        display.grid(row=0, column=0, columnspan=5)
-
+        if self.keys == "0":
+            self.keys = str(key)
+            print(str(key))
+        else:
+           self.keys = str(self.keys) + str(key)
+           print(str(key))
+        self.C.delete(ALL)
+        self.C.create_text(240, 40, font=("Purisa", 30), text=self.keys, justify=RIGHT)
+        #self.say(key)
 
     #set Buttons
     def set_buttons(self):
@@ -16,7 +37,7 @@ class calc_core:
         for x in keys:
             if x == 'sum':
                 buttons = Button(text="=", width=13, height=3, relief=RIDGE, font=("Courier New", 8, 'bold'), bd=2,
-                                 command=lambda: self.press_me(sum))
+                                 command=lambda: self.press_me('equal'))
                 buttons.config(font=('Courier New', 16, 'bold'), bg='#ffbf00', fg="white")
                 buttons.grid(row=n_row, column=n_col, columnspan=2, pady=3, padx=3)
             else:
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
-import core_class
+from py_calculator.calc_core import calc_core
 
-class calculator(core_class.calc_core):
+class calculator():
     # initializing Tkinter & Setting WindowRIDGE
     fkt = Tk()
     fkt.geometry("375x570")
@@ -9,15 +9,11 @@ class calculator(core_class.calc_core):
     fkt.iconbitmap("calculator.ico")
     fkt.configure(bg="white")
     fkt.resizable(0, 0)
-    #Set Welcome Screen
-    C = Canvas(fkt, bd=2, bg="gray", height=80, width=340)
-    C.create_text(240, 40, font=("Purisa", 30), justify=RIGHT, text="Hello World!")
-    C.delete(ALL)
-    C.create_text(240, 40, font=("Purisa", 30), text="Hi!", justify=RIGHT)
-    C.grid(rowspan=1, columnspan=5, padx=1, pady=15)
 
     #call core engine
-    engine = core_class.calc_core()
+    engine = calc_core()
+    engine.dispaly(fkt)
     engine.set_buttons()
+
     #call Gui
     fkt.mainloop()
